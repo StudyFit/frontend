eb7bc799baf74b6bda18ca0aabd4dddde62fa648
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-05-14 15:16:22 +0900

    [Add] 오늘의 수업 화면 구현에 필요한 아이콘 추가

--- assets/images/todays-lesson/index.js
+++ assets/images/todays-lesson/index.js
@@ -1,6 +1,12 @@
 export const todaysLessonImages = {
   dateLeftBtn: require("./date_change_arrow_left.png"),
   dateRightBtn: require("./date_change_arrow_right.png"),
+
+  notificationBtn: require("./notification.png"),
+  alarmBtn: require("./alarm.png"),
+
   hwBeforeUpload: require("./hw_before_upload.png"),
+  hwAfterUpload: require("./hw_after_upload.png"),
   hwCheckFalse: require("./hw_check_false.png"),
+  hwCheckTrue: require("./hw_check_true.png"),
 };
