6f29a1281e89ec93b572d7643fd6bd4d849741af
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-06-07 23:52:28 +0900

    [Feat] 컬러팔레트 지정

---
+++ assets/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  lightPink: "#FFB5DE",
+  pink: "#FC99C7",
+  coral: "#FDB786",
+  orange: "#FBCC62",
+  yellow: "#FDED91",
+  lightGreen: "#D3ED70",
+  green: "#A7DF91",
+  blue: "#BBD9FB",
+  lavender: "#A6A8FB",
+  purple: "#D3BDFE",
+};
+
+export default colors;
