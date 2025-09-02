dd34d1e07cda9832f9fee6aff541e049ce68c6f9
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-09-02 15:26:29 +0900

    [Add] 로그아웃 시 확인 모달창 띄우기

--- components/MyPage/LogoutBtn.jsx
+++ components/MyPage/LogoutBtn.jsx
@@ -1,11 +1,17 @@
 import { useUser } from "@/contexts/UserContext";
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Alert, Pressable, StyleSheet, Text } from "react-native";
 
 const LogoutBtn = () => {
   const { logout } = useUser();
 
+  const handleLogout = () => {
+    Alert.alert("로그아웃", "정말 로그아웃 하시겠습니까?", [
+      { text: "취소", style: "cancel" },
+      { text: "확인", onPress: logout },
+    ]);
+  };
   return (
-    <Pressable onPress={logout} style={styles.logoutBtn}>
+    <Pressable onPress={handleLogout} style={styles.logoutBtn}>
       <Text style={styles.logoutText}>로그아웃</Text>
     </Pressable>
   );
