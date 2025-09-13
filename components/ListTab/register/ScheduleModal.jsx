65535943c61a8a89dabd91e217c634f77dd381fc
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-09-13 21:54:57 +0900

    [Feat] 학생 추가 시 수업 일정 등록 모달창 구현

--- components/ListTab/register/ScheduleModal.jsx
+++ components/ListTab/register/ScheduleModal.jsx
@@ -1,9 +1,79 @@
 import { registerIcon } from "@/assets";
+import { ScheduleTimeInput } from "@/components/Calendar";
 import { BottomBtn } from "@/components/login";
-import React from "react";
-import { Image, Modal, Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Image,
+  Modal,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+const daysOfWeek = [
+  ["월", "MONDAY"],
+  ["화", "TUESDAY"],
+  ["수", "WEDNESDAY"],
+  ["목", "THURSDAY"],
+  ["금", "FRIDAY"],
+  ["토", "SATURDAY"],
+  ["일", "SUNDAY"],
+];
 
 const ScheduleModal = ({ onRequestClose, schedule, setSchedule }) => {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [selectedDay, setSelectedDay] = useState(daysOfWeek[0][0]);
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [classTimeDtoList, setClassTimeDtoList] = useState([]);
+  const [error, setError] = useState("");
+
+  const selectDay = () => {
+    const currentIndex = daysOfWeek.findIndex((day) => day[0] === selectedDay);
+    const nextIndex = (currentIndex + 1) % daysOfWeek.length;
+    setSelectedDay(daysOfWeek[nextIndex][0]);
+  };
+
+  const addSchedule = () => {
+    if (!startTime || !endTime) return;
+
+    const day =
+      daysOfWeek[daysOfWeek.findIndex((day) => day[0] === selectedDay)][1];
+
+    const classTimeDto = {
+      day,
+      startTime,
+      endTime,
+    };
+    // 중복 검사
+    const isDuplicate = classTimeDtoList.some(
+      (item) =>
+        item.day === classTimeDto.day &&
+        item.startTime === classTimeDto.startTime &&
+        item.endTime === classTimeDto.endTime
+    );
+    if (isDuplicate) {
+      setError("이미 같은 요일과 시간이 추가되어 있습니다.");
+      return;
+    }
+    setClassTimeDtoList((prev) => [...prev, classTimeDto]);
+
+    // 초기화
+    setStartDate("");
+    setEndDate("");
+    setSelectedDay(daysOfWeek[0][0]);
+    setStartTime("");
+    setEndTime("");
+    setError("");
+  };
+
+  const deleteSchedule = (index) =>
+    setClassTimeDtoList((prev) => prev.filter((_, i) => i !== index));
+
   return (
     <Modal transparent animationType="slide" onRequestClose={onRequestClose}>
       <Pressable style={styles.overlay} onPress={onRequestClose}>
@@ -17,6 +87,135 @@ const ScheduleModal = ({ onRequestClose, schedule, setSchedule }) => {
             <Text style={styles.headerText}>수업 일정</Text>
           </View>
 
+          <View style={{ gap: 7, marginBottom: 19 }}>
+            <Text style={{ fontSize: 20 }}>수업 기간</Text>
+            <View
+              style={{
+                flexDirection: "row",
+                gap: 10,
+                alignItems: "center",
+                gap: 15,
+              }}
+            >
+              <TextInput
+                value={startDate}
+                onChangeText={setStartDate}
+                style={styles.textInput}
+              />
+              <Image
+                source={registerIcon.dashIcon}
+                style={{ width: 24, height: 24 }}
+              />
+              <TextInput
+                value={endDate}
+                onChangeText={setEndDate}
+                style={styles.textInput}
+              />
+            </View>
+          </View>
+
+          <View style={{ gap: 5, marginBottom: 20 }}>
+            <Text style={{ fontSize: 20 }}>수업 요일</Text>
+            <View style={{ flexDirection: "row", gap: 10 }}>
+              <Pressable
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: "100%",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#F0F0F0",
+                }}
+                onPress={selectDay}
+              >
+                <Text style={{ fontSize: 16 }}>{selectedDay}</Text>
+              </Pressable>
+              <View style={{ flex: 1 }}>
+                <ScheduleTimeInput
+                  startTime={startTime}
+                  setStartTime={setStartTime}
+                  endTime={endTime}
+                  setEndTime={setEndTime}
+                  visible={true}
+                />
+              </View>
+            </View>
+
+            <Pressable
+              style={{
+                height: 40,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#DDDDDD",
+                borderRadius: 4,
+              }}
+              onPress={addSchedule}
+            >
+              <Text style={{ fontSize: 16, color: "#555555" }}>+ 추가하기</Text>
+            </Pressable>
+          </View>
+          <ScrollView>
+            <View style={{ gap: 12 }}>
+              {classTimeDtoList.map((classDto, index) => {
+                const day =
+                  daysOfWeek[
+                    daysOfWeek.findIndex((day) => day[1] === classDto.day)
+                  ][0];
+
+                return (
+                  <View
+                    key={index}
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      paddingHorizontal: 10,
+                    }}
+                  >
+                    <Text style={[{ fontSize: 16 }, { marginRight: 14 }]}>
+                      {day}
+                    </Text>
+                    <Text style={[{ fontSize: 16 }, { marginRight: "auto" }]}>
+                      {classDto.startTime} ~ {classDto.endTime}
+                    </Text>
+                    <Pressable
+                      style={{
+                        width: 48,
+                        height: 26,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        borderRadius: 10,
+                        borderWidth: 1.3,
+                        borderColor: "red",
+                      }}
+                      onPress={() => deleteSchedule(index)}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 11,
+                          fontFamily: "Pretendard-Bold",
+                          color: "red",
+                        }}
+                      >
+                        삭제
+                      </Text>
+                    </Pressable>
+                  </View>
+                );
+              })}
+            </View>
+          </ScrollView>
+          {error && (
+            <Text
+              style={{
+                color: "red",
+                alignSelf: "center",
+                fontSize: 15,
+                marginTop: 15,
+              }}
+            >
+              {error}
+            </Text>
+          )}
           {/* Save Button */}
           <BottomBtn
             text="저장하기"
@@ -40,7 +239,6 @@ const styles = StyleSheet.create({
   },
   modalBox: {
     width: "100%",
-    height: 300,
     paddingHorizontal: 27,
     paddingTop: 24,
     paddingBottom: 16,
@@ -52,6 +250,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     gap: 8,
+    marginBottom: 15,
   },
   headerIcon: { width: 30, height: 30 },
   headerText: { fontSize: 20, fontFamily: "Pretendard-Bold" },
@@ -70,5 +269,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   checkIcon: { width: 20, height: 20 },
-  saveBtn: { width: "100%" },
+  saveBtn: { width: "100%", marginTop: 16 },
+
+  textInput: { flex: 1, height: 40, backgroundColor: "#F0F0F0" },
 });
